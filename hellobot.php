25415f5af46321000cd531510807af0fd03f52e2
Reda <reda.otb@gmail.com>
2020-10-23 15:24:25 +0100

    Update hellobot.php

--- hellobot.php
+++ hellobot.php
@@ -1,9 +1,3 @@
 <?
-  $input = $_GET["query"];
-  $nlp = explode(" ", $input);
-  foreach ($nlp as $keyword) {
-    if ($keyword == "hello" || $keyword == "Hello" || $keyword == "helo" || $keyword == "hi" || $keyword == "good day") {
-      echo "Hello I'm the chatbot.";
-    }
-  }
+
 ?>
